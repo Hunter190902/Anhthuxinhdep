f57099ad176853f37f0241b36e86a2b0c9bd6eca
LoveLn <thinh.phamyamer0@hcmut.edu.vn>
2022-12-06 13:35:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Nga yêu Thịnh và Thịnh cũng yêu Nga mãi mãi nhoaaaa <33333333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Để tôi trao em những điều trân thành và dịu dàng nhất.',
     btnAccept: 'Okiiiii lun <3',
