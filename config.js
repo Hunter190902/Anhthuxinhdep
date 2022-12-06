9ab59fc75f9bacb289bd55f2f0457470c4fc5251
LoveLn <thinh.phamyamer0@hcmut.edu.vn>
2022-12-06 14:33:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,3 +13,5 @@ const CONFIG = {
     mess: 'Tớ biết mà. Tớ sẽ cố gắng để tỏ tình và mang lại cho cô ấy hạnh phúc nhiều ơi là nhiều ',
     messDesc: 'Để tớ trao cô ấy những điều trân thành và dịu dàng nhất.',
     btnAccept: 'Okiiiii lun <3'.
+
+}
