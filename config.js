3492f178c4a5a8d756fbdfd3d98f004e57737de3
Hunter190902 <128292963+Hunter190902@users.noreply.github.com>
2023-03-19 18:50:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    titleWeb: "Gửi cho người nào đó",
-    introTitle: 'Hình như tớ thích 1 người rồi áaaa',
-    introDesc: `Cô ấy khom như gu tớ, hay khóc nè, hay overthinking, lại còn là 1 nữ cường nựa... nhma cô ấy gất là dễ thương nhaaaaaa với quan tâm tớ nựa, xỉu mất hoi écccc`,
-    btnIntro: '^^HiHi^^',
-    title: 'Cậu biết hem, tớ thích cô ấy vì cô ấy y như tớ hồi đấy vậy, đềuu mất niềm tin vào tình yêu, aydaaaa',
-    desc: 'Nhma nếu cậu là cô ấy, thì cậu có thích 1 con người hay quan tâm thái quá và simp quá mức không, có thể sẽ phiền đó huhu',
-    btnYes: 'Vẫn cứ là thích nhaaaaaa <33',
-    btnNo: 'Không, cậu quá trẻ con',
-    question: 'Ơ, tại sao đấyyyyyyyy, tớ có gì tốt đâu mà',
-    btnReply: 'Cô ấy có rất rất nhiều điểm tốt mà, như bầu trời đầy sao lun á, nhưng mà tớ có nên tỏ tình khom nhỉ, huhu?',
-    reply: 'cóoooooooooo ',
-    mess: 'Tớ biết mà. Tớ sẽ cố gắng để tỏ tình và mang lại cho cô ấy hạnh phúc nhiều ơi là nhiều ',
-    messDesc: 'Để tớ trao cô ấy những điều trân thành và dịu dàng nhất.',
-    btnAccept: 'Okiiiii lun <3'.
-    messLink, 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-:
-}
-
+    titleWeb: "AnhThuxinhnhat",
+    introTitle: 'Wowww, hình như cóa nàng công túa xinh đẹp nào đósinh ngày 19/4 bật web hả taa! ',
+    introDesc: `Nhma cô ấy hay tự ôm việc nìa, hong có nghĩ đến sức khỏe nè, đôi khi còn tin người quá mức nựaa, còn nhiều lúc bảo là mình gất hung dữ mà hiền khô để cho ngta ăn hiếp hong lun ó :<`,
+    btnIntro: 'hic hic, có hả ',
+    title: 'Aizaa, con bé công túa nì toàn làm ngta lo kh àa, nhiều lúc hong bíc làm sao lun.',
+    desc: 'Nhma chính vì điều đó lại khiến nàng công túa ấy trở nên xinh đẹp hơn bất cứ ai, cô ấy trở thành phiên bản của riêng cô ấy, điều mà có thể nói là nét đẹp nhất của cô ấy. À mà tự nhiên tui muốn kí đầu con bé công túa ấy ghê :))) ',
+    btnYes: 'Tạm tha đó =3',
+    btnNo: 'Bn tòyyyy',
+    question: 'Ụa sao nay tha cho tui zị, má ưiiii chuyện lạ à nhaaaa',
+    btnReply: 'Éc Ô Éc',
+    reply: 'tại Anh Thư hứa từ giờ sẽ kh chửi Hưng nựa',
+    mess: 'Chời Ưiiiii lạ zị ta',
+    messDesc: 'Nhưng mà chúc mừng sinh nhật 20 tủi anhthu nhaa, chúc cho nàng công túa anhthu có cuộc sống bình an, an yên, thẳng thắn mà chân thành, nhiều may mắn và không thị phi. Chúc cho thời gian có thể mãi chầm chậm không vội vàng , chúc cho hạnh phúc có thể mãi không rời bỏ mãi không buông tay. Chúc cho nụ cười của Thu luôn luôn nở trên môi. Và hơn hết chào mừng Ceo qua tuổi 20 đầy nhiệt huyết thanh xuân ',
+    btnAccept: 'nhấn ok là qua mess tui nà =3',
+    messLink: 'https://www.facebook.com/NgQDinhHung'
